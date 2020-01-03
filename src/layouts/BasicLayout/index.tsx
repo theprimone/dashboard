ac64cef0d0da612d2a241586c6a3ed08346d3663
Yuns Xie <yuns.xie@qq.com>
2020-01-03 09:41:40 +0800

    feat: setOctokit

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,14 +1,17 @@
 import React, { useContext } from 'react';
 import GlobalContext from '@/components/GlobalContext/context';
 import { useCode } from '@/utils/hooks/users';
+import Octokit from '@octokit/rest';
 
 const BasicLayout: React.FC = props => {
   const { setOctokit } = useContext(GlobalContext);
   const [code, token] = useCode();
-  console.log(code, token);
 
-  if (code) {
+  if (code && !token) {
     return <p>Get token...</p>;
+  }
+  if (token) {
+    setOctokit(new Octokit({ auth: token }));
   }
 
   return (
