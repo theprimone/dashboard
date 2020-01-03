c4de2a40e4417118727aea3e8188e60aa2b5f445
Yuns Xie <yuns.xie@qq.com>
2020-01-03 09:44:53 +0800

    feat: extract consts

--- src/services/authorize.ts
+++ src/services/authorize.ts
@@ -1,8 +1,8 @@
-import { CLIENT_ID, CLIENT_SECRET } from '@/utils/constants';
+import { CLIENT_ID, CLIENT_SECRET, CORS_PROXY_URL, GET_TOKEN_URL } from '@/utils/constants';
 import request from '@/utils/request';
 
 export async function getToken(code: string) {
-  return await request("https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token", {
+  return await request(`${CORS_PROXY_URL}/${GET_TOKEN_URL}`, {
     method: "POST",
     data: {
       code,
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,5 @@
 export const CLIENT_ID = "fba2176a59765757bbf9";
 export const CLIENT_SECRET = "be8e76bb1dbb43d25fbe1731485720eeca702f0f";
+
+export const CORS_PROXY_URL = "https://cors-anywhere.herokuapp.com";
+export const GET_TOKEN_URL = "https://github.com/login/oauth/access_token";
