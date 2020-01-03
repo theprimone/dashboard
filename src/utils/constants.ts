463ba45cc152fb29aaa5398f3ea3ad4b42f24618
Yuns Xie <yuns.xie@qq.com>
2020-01-03 09:33:46 +0800

    chore: remove useless

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,2 @@
-import Octokit from '@octokit/rest';
-
 export const CLIENT_ID = "fba2176a59765757bbf9";
 export const CLIENT_SECRET = "be8e76bb1dbb43d25fbe1731485720eeca702f0f";
-
-export const clientWithAuth = new Octokit({
-  auth: "TOKEN",
-});
