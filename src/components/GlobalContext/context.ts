9ad2983336d0374318f9d1b32d7edc5176ad200b
Yuns Xie <yuns.xie@qq.com>
2020-01-04 11:19:36 +0800

    feat: use setDefaultGlobalData

--- src/components/GlobalContext/context.ts
+++ src/components/GlobalContext/context.ts
@@ -1,16 +1,19 @@
 import React from 'react';
+// import moment from 'moment';
 import Octokit from '@octokit/rest';
 
 export const isAuthorised = () => !!localStorage.getItem("TOKEN");
 
 export const setDefaultGlobalData = () => ({
   authorised: isAuthorised(),
+  // currentYear: moment().get('year'),
   octokit: new Octokit(isAuthorised ? { auth: localStorage.getItem("TOKEN")! } : undefined),
   setOctokit: () => { },
 })
 
 export interface GlobalConsumerProps {
   authorised: boolean;
+  currentYear?: number;
   /**
    * ref: https://octokit.github.io/rest.js
    */
@@ -18,11 +21,7 @@ export interface GlobalConsumerProps {
   setOctokit: (octokit: Octokit) => void;
 }
 
-export const GlobalContext = React.createContext<GlobalConsumerProps>({
-  authorised: false,
-  octokit: new Octokit(),
-  setOctokit: () => { },
-});
+export const GlobalContext = React.createContext<GlobalConsumerProps>(setDefaultGlobalData());
 
 GlobalContext.displayName = 'Global ConfigContext';
 
