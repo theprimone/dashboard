5da9aac7b7056dba522c2eeceb654a907ff12227
Yuns Xie <yuns.xie@qq.com>
2020-01-05 21:27:50 +0800

    refactor: use setOctokit

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,22 +1,26 @@
 import React, { useContext } from 'react';
 import router from 'umi/router';
 import GlobalContext from '@/components/GlobalContext/context';
+import setOctokit from '@/utils/octokit';
 import { useCode } from '@/utils/hooks/users';
-import Octokit from '@octokit/rest';
 
 const BasicLayout: React.FC = props => {
-  const { setOctokit } = useContext(GlobalContext);
+  const { setOctokit: setGlobalOctokit, authorised } = useContext(GlobalContext);
   const [code, token, state] = useCode();
 
   if (code) {
     if (state.loading) {
       return <p>Get token...</p>;
     } else if (token) {
-      setOctokit(new Octokit({ auth: token }));
+      setGlobalOctokit(setOctokit({ auth: token }));
       localStorage.setItem("TOKEN", token);
-      router.replace('/');
+      router.replace('/dashboard');
     }
     return <p>Get token failed.</p>
+  }
+
+  if (authorised) {
+    router.push('/dashboard');
   }
 
   return (
