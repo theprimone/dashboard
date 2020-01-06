0eabda7b9d99950ed6eaf75dd4e891b20794ca9a
Yuns Xie <yuns.xie@qq.com>
2020-01-06 23:21:35 +0800

    refactor && feat: extract sumArray and renderGrid && forksCount

--- src/business-components/Repos/index.tsx
+++ src/business-components/Repos/index.tsx
@@ -10,9 +10,15 @@ import {
 } from '@material-ui/core';
 import BookIcon from '@material-ui/icons/Book';
 import StarIcon from '@material-ui/icons/Star';
+import CallSplitIcon from '@material-ui/icons/CallSplit';
 import Spin from '@/components/Spin';
 import GlobalContext from '@/components/GlobalContext/context';
 import { useRepos, ReposListItem } from '@/utils/hooks/repos';
+
+function sumArray<T>(arr: T[], field: string) {
+  const reducer = (accumulator: number, currentValue: T) => accumulator + (currentValue as any)?.[field];
+  return arr.reduce(reducer, 0);
+}
 
 const useStyles = makeStyles(() => createStyles({
   icon: {
@@ -25,13 +31,24 @@ export default function Repos() {
   const { value, loading } = useRepos();
 
   const { data } = value || {};
-  const starCount = () => {
-    if (data) {
-      const reducer = (accumulator: number, currentValue: ReposListItem) => accumulator + currentValue.stargazers_count;
-      return data.reduce(reducer, 0);
-    }
-    return 0;
+  const starsCount = () => data ? sumArray(data, 'stargazers_count') : 0;
+
+  const forksCount = () => data ? sumArray(data, 'forks_count') : 0;
+
+  const renderGrid = (node: JSX.Element) => {
+    return (
+      <Grid
+        item
+        xs={6}
+        sm={4}
+        md={3}
+        lg={1}
+      >
+        {node}
+      </Grid>
+    )
   }
+
   return (
     <Spin spinning={loading}>
       <Card>
@@ -40,30 +57,24 @@ export default function Repos() {
         />
         <CardContent>
           <Grid container>
-            <Grid
-              item
-              xs={6}
-              sm={4}
-              md={3}
-              lg={1}
-            >
+            {renderGrid(
               <Typography>
                 <BookIcon className={classes.icon} />
                 {data ? data.length : 0}
               </Typography>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              sm={4}
-              md={3}
-              lg={1}
-            >
+            )}
+            {renderGrid(
               <Typography>
                 <StarIcon className={classes.icon} />
-                {starCount()}
+                {starsCount()}
               </Typography>
-            </Grid>
+            )}
+            {renderGrid(
+              <Typography>
+                <CallSplitIcon className={classes.icon} />
+                {forksCount()}
+              </Typography>
+            )}
           </Grid>
         </CardContent>
       </Card>
