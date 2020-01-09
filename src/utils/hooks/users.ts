ce791297c7a76ac59c56adcfb5594ab4a51fbc41
Yuns Xie <yuns.xie@qq.com>
2020-01-09 23:15:18 +0800

    refactor: set token

--- src/utils/hooks/users.ts
+++ src/utils/hooks/users.ts
@@ -2,18 +2,23 @@ import { useContext } from 'react';
 import { useAsync } from 'react-use';
 import { AsyncState } from 'react-use/lib/useAsync';
 import GlobalContext from '@/components/GlobalContext/context';
-import { parse } from 'qs';
 import { getToken, GetTokenResponse } from '@/services/authorize';
 
-export function useCode(): [string, string | undefined, AsyncState<GetTokenResponse>] {
-  const { code } = parse(location.search.replace(/^\?/, ''));
+export function useCode(code: string): [string | undefined, AsyncState<GetTokenResponse>] {
+  const { setOctokit: setGlobalOctokit } = useContext(GlobalContext);
+
   let state: AsyncState<GetTokenResponse> = { loading: true };
   if (code) {
     state = useAsync(async () => await getToken(code), [code]);
   }
   const token = state?.value?.access_token;
 
-  return [code, token, state];
+  if (token) {
+    setGlobalOctokit({ auth: token });
+    localStorage.setItem("TOKEN", token);
+  }
+
+  return [token, state];
 }
 
 export function useUserInfo() {
